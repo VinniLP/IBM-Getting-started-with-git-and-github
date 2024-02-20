cf908afb48fba4b4f1d637b475606b7ea11c13ce
VinniLP <dellvinni@gmail.com>
2024-02-20 21:40:34 +0530

    Welcome Page

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('enter-btn').addEventListener('click', function() {
+    // You can customize this action according to your requirements
+    alert('Welcome! You have entered the website.');
+  });
+  
